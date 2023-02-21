262dd9a41f156d4c6b5fa577f50bb78300e61445
Matthew <matthew.kluber@gmail.com>
2023-02-20 19:36:06 -0600

    shift setPosition to a tweening animation instead of a snap

--- Assets/Scripts/Grid/GamePiece.cs
+++ Assets/Scripts/Grid/GamePiece.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using TacticsX.Grid;
+using DG.Tweening;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+using UnityEngine.UIElements;
 
 namespace TacticsX.GridImplementation
 {
     public class GamePiece : Node
     {
         private GameObject gameObject;
+        private float tweenTime = 0.25f;
 
         public GamePiece(GameObject prefab)
         {
@@ -25,13 +29,14 @@ namespace TacticsX.GridImplementation
 
         public override void SetPosition(float x, float y)
         {
-            gameObject.transform.position = new Vector3(x, 0, y);
+            this.gameObject.transform.DOMove(new Vector3(x, 0, y), tweenTime).SetEase(Ease.OutCirc);
         }
 
         public void SetPosition(Vector3 position)
         {
             SetPosition(position.x, position.z);
         }
+
 
         public virtual void DoAction(){}
     }
