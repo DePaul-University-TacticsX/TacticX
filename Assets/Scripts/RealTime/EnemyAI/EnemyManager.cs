a9421d98dd3a6671f8ae193dfc47bad815f8627c
HarkChim <bensonharkison10@hotmail.com>
2023-03-05 21:50:31 -0600

    ai moves a little better

--- Assets/Scripts/RealTime/EnemyAI/EnemyManager.cs
+++ Assets/Scripts/RealTime/EnemyAI/EnemyManager.cs
@@ -10,15 +10,15 @@ public class EnemyManager : MonoBehaviour {
     
     // speed (in between warrior and archer), range, position
     
-    EnemyAI.SetEnemy(new Enemy(3.5f, 0.3f, new Vector3(-5, height, 0)));
-    EnemyAI en1 = Object.Instantiate<EnemyAI>(Resources.Load<EnemyAI>("Prefabs/Demon"));
+    // EnemyAI.SetEnemy(new Enemy(3.5f, 0.3f, new Vector3(-5, height, 0)));
+    // EnemyAI en1 = Object.Instantiate<EnemyAI>(Resources.Load<EnemyAI>("Prefabs/Demon"));
     
-    // this demon is slightly slower
-    EnemyAI.SetEnemy(new Enemy(3.0f, 0.3f, new Vector3(5, height, -20)));
-    EnemyAI en2 = Object.Instantiate<EnemyAI>(Resources.Load<EnemyAI>("Prefabs/Demon"));
+    // // this demon is slightly slower
+    // EnemyAI.SetEnemy(new Enemy(3.0f, 0.3f, new Vector3(5, height, -20)));
+    // EnemyAI en2 = Object.Instantiate<EnemyAI>(Resources.Load<EnemyAI>("Prefabs/Demon"));
     
     // barbarian is much slower
-    EnemyAI.SetEnemy(new Enemy(1.5f, 0.3f, new Vector3(-5, height, -20)));
+    EnemyAI.SetEnemy(new Enemy(0.75f, 0.3f, new Vector3(-5, height, -20)));
     EnemyAI en3 = Object.Instantiate<EnemyAI>(Resources.Load<EnemyAI>("Prefabs/Barbarian"));
   }
 
